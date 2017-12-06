a39c0799da3ae1e19674f595a0857f3cfc267e9e
noorannooran <nooran.elsherif@gmail.com>
2017-12-06 11:29:38 -0500

    small changes

--- __main__.py
+++ __main__.py
@@ -2,10 +2,9 @@ import ciphers
 from ciphers import caesar_cipher
 
 def main():
-    cypher2 =  caesar_cipher.Caesar()
+    cypher =  caesar_cipher.Caesar()
     message = "... hello this is a TEST with punctuation; : and with capitals!?#$"
-
-    cypher2.print_translations(message)
-
+    cypher.print_translations(message)
+    
 if __name__=="__main__":
     main()
